2b9131e8c71eccbc029b836661f616773fa1936a
RikkiGenack <rgenack@student.touro.edu>
2026-02-11 15:35:58 -0500

    Update Gradebook.java

--- src/main/java/edu/course/gradebook/Gradebook.java
+++ src/main/java/edu/course/gradebook/Gradebook.java
@@ -13,34 +13,116 @@ public class Gradebook {
     }
 
     public boolean addStudent(String name) {
-        throw new UnsupportedOperationException();
+        if(gradesByStudent.containsKey(name)) {
+            activityLog.add("Student "+ name +" already exists");
+            return false;
+        }
+        gradesByStudent.put(name, new ArrayList<Integer>());
+        return true;
     }
 
     public boolean addGrade(String name, int grade) {
-        throw new UnsupportedOperationException();
+        if(!gradesByStudent.containsKey(name)) {
+            activityLog.add("Student " + name + " not found");
+            return false;
+        }
+        gradesByStudent.get(name).add(grade);
+        //push an action onto undo stack to remove this grade
+        UndoAction undo = gradebook -> {gradesByStudent.get(name).remove(grade);};
+        undoStack.push(undo);
+
+
+        return true;
+
     }
 
     public boolean removeStudent(String name) {
-        throw new UnsupportedOperationException();
+        if(gradesByStudent.containsKey(name)) {
+            gradesByStudent.remove(name);
+            UndoAction undo = gradebook -> {addStudent(name);};
+            undoStack.push(undo);
+            activityLog.add("student "+ name +" removed");
+            return true;
+        }
+        return false;
     }
 
     public Optional<Double> averageFor(String name) {
-        throw new UnsupportedOperationException();
+        List<Integer> grades = gradesByStudent.get(name);
+        int sum = 0;
+        for(Integer grade : grades) {
+            sum += grade;
+        }
+        Double average = (double) (sum/grades.size());
+        if(grades.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(average);
     }
 
     public Optional<String> letterGradeFor(String name) {
-        throw new UnsupportedOperationException();
+
+       Optional <Double> avg = averageFor(name);
+       Double doubleAvg = avg.get();
+       if(avg.isPresent()) {
+           String grade = switch (doubleAvg) {
+               case Double d when d >= 90 ->{
+                   yield "A";
+               }
+               case Double d  when d >=80 &&d<90  -> {
+                   yield "B";
+               }
+               case Double d when d >= 70 && d <80 -> {
+                   yield "C";
+               }
+               case Double d when d >= 60 && d <70 -> {
+                   yield "D";
+               }
+               default   -> "F";
+           };
+           return Optional.of(grade);
+       }
+       return Optional.empty();
     }
 
     public Optional<Double> classAverage() {
-        throw new UnsupportedOperationException();
+        int sum = 0;
+        int studentAmt =0;
+        Students:
+        for(String student : gradesByStudent.keySet() ) {
+            studentAmt++;
+            List<Integer> grades = gradesByStudent.get(student);
+            StudentsGrades:
+            for(Integer grade: grades){
+                sum += grade;
+            }
+
+        }
+        Double average = (double) (sum/studentAmt);
+        if(gradesByStudent.isEmpty()) {
+            return Optional.empty();
+        }
+      return Optional.of(average);
     }
 
     public boolean undo() {
-        throw new UnsupportedOperationException();
+        if(undoStack.isEmpty()) {
+            return false;
+        }
+        UndoAction last = undoStack.pop();
+        last.undo(this);
+        activityLog.push("Undid last action");
+        return true;
+
     }
 
     public List<String> recentLog(int maxItems) {
-        throw new UnsupportedOperationException();
+       //I wasn't sure how to use the descending Iterator so I had AI give me a similar example and then I adpated it
+        Iterator<String> descendingIterator = activityLog.descendingIterator();
+       List<String> log =  new ArrayList<>();
+       while(descendingIterator.hasNext()&&log.size()<maxItems) {
+           log.add(descendingIterator.next());
+       }
+        return log;
     }
 }
